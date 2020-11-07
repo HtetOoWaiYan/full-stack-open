e981fecdeee82b84ad73977629da05f0a84a89a2
HtetOoWaiYan <htetoo.waiyan@outlook.com>
2020-11-07 21:15:37 +0630

    Add MongoDB Atlas connection string

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -13,7 +14,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
@@ -37,7 +38,7 @@ app.post('/api/blogs', (request, response) => {
 		})
 })
 
-const PORT = 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
