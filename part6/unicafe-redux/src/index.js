dfa176361400abb6568dec20952b5b4107abe22d
HtetOoWaiYan <htetoo.waiyan@outlook.com>
2020-11-27 19:53:48 +0630

    Add Ex. 6.1-2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -6,27 +6,37 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+	const good = () => {
+		store.dispatch({ type: 'GOOD' })
+	}
 
-  return (
-    <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
-    </div>
-  )
+	const ok = () => {
+		store.dispatch({ type: 'OK' })
+	}
+
+	const bad = () => {
+		store.dispatch({ type: 'BAD' })
+	}
+
+	const reset = () => {
+		store.dispatch({ type: 'ZERO' })
+	}
+
+	return (
+		<div>
+			<button onClick={good}>good</button>
+			<button onClick={ok}>neutral</button>
+			<button onClick={bad}>bad</button>
+			<button onClick={reset}>reset stats</button>
+			<div>good {store.getState().good}</div>
+			<div>neutral {store.getState().ok}</div>
+			<div>bad {store.getState().bad}</div>
+		</div>
+	)
 }
 
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+	ReactDOM.render(<App />, document.getElementById('root'))
 }
 
 renderApp()
